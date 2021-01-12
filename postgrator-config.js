40f6bbd32c90a801889d0e94701a8039362cdc9c
Andrew Schmit <schmita91@gmail.com>
2021-01-11 22:46:28 -0500

    initial commit

---
+++ postgrator-config.js
@@ -0,0 +1,12 @@
+
+require('dotenv').config();
+
+module.exports = {
+  migrationDirectory: 'migrations',
+  ssl: { rejectUnauthorized: true },
+  driver: 'pg',
+  connectionString: 
+    process.env.NODE_ENV === 'test'
+      ? process.env.TEST_DATABASE_URL
+      : process.env.DATABASE_URL,
+};
