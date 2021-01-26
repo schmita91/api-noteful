fe7749bf9e4123b316240c10c114f9ac6919c045
Andrew Schmit <schmita91@gmail.com>
2021-01-26 11:20:54 -0500

    connection strings

--- src/server.js
+++ src/server.js
@@ -1,10 +1,10 @@
 const app = require('./app')
-const { PORT, DATABASE_URL } = require('./config')
+const { PORT, CONNECTION_STRING } = require('./config')
 const knex = require('knex')
 
 const db = knex({
   client:'pg',
-  connection: DATABASE_URL,
+  connection: CONNECTION_STRING,
   ssl: {
     rejectUnauthorized: false
   }
