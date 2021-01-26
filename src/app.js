a85c37dfef5a469367330cc1a4d67e8d1d42f243
Andrew Schmit <schmita91@gmail.com>
2021-01-26 10:43:31 -0500

    undoing api routes

--- src/app.js
+++ src/app.js
@@ -17,8 +17,8 @@ app.use(morgan(morganOption))
 app.use(helmet())
 app.use(cors())
 
-app.use('api/folders', foldersRouter)
-app.use('api/notes', notesRouter)
+app.use('/api/folders', foldersRouter)
+app.use('/api/notes', notesRouter)
 
 app.use(function errorHandler(error, req, res, next) {
     let response
