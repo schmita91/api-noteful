0eadcb04f0a8927dad24e54a1af3baf8d6025f63
Andrew Schmit <schmita91@gmail.com>
2021-02-05 15:37:09 -0500

    db urls

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DATABASE_URL: process.env.DATABASE_URL || 'postgresql://postgres@localhost/noteful'
+  DATABASE_URL: 'postgres://zfzxbbnq:RClco3A6FDybqj_xEk2NoQVByzOlMlLR@ziggy.db.elephantsql.com:5432/zfzxbbnq'
 }
